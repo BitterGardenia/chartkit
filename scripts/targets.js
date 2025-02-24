4fb8faa298e141471ec2f023169aa78d915dbb4f
袁照阳 <alwaysmissly@163.com>
2025-02-24 16:28:18 +0800

    build: 过滤`package.json`包含`private: true`的包

--- scripts/targets.js
+++ scripts/targets.js
@@ -1,11 +1,10 @@
 import fs from 'node:fs'
 
-export default fs.readdirSync('packages').filter((f) => {
-  if (!fs.statSync(`packages/${f}`).isDirectory() || !fs.existsSync(`packages/${f}/package.json`)) {
-    return false
-  }
-  const pkg = fs.readFileSync(`packages/${f}/package.json`)
-  if (pkg.private && !pkg.buildOptions) {
+export default fs.readdirSync('packages').filter(f => {
+  if (
+    !fs.statSync(`packages/${f}`).isDirectory() ||
+    !fs.existsSync(`packages/${f}/package.json`)
+  ) {
     return false
   }
   return true
